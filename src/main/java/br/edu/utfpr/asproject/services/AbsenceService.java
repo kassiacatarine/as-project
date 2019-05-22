832f72c05763f09f9dae5cd5412d4f0c64c4dc8e
Kássia Catarine <kassia_catarine.15@hotmail.com>
2019-05-22 19:41:17 -0300

    Adicionando serviço
    Signed-off-by: Kássia Catarine <kassia_catarine.15@hotmail.com>

--- src/main/java/br/edu/utfpr/asproject/services/AbsenceService.java
+++ src/main/java/br/edu/utfpr/asproject/services/AbsenceService.java
@@ -5,10 +5,30 @@
  */
 package br.edu.utfpr.asproject.services;
 
+import br.edu.utfpr.asproject.entities.Absence;
+import br.edu.utfpr.asproject.repositories.AbsenceRepository;
+
 /**
  *
  * @author akira
  */
 public class AbsenceService {
 
+    private AbsenceRepository absenceRepository;
+
+    public AbsenceService(AbsenceRepository absenceRepository) {
+        this.absenceRepository = absenceRepository;
+    }
+
+    public void createAbsence(Absence absence) {
+        absenceRepository.save(absence);
+    }
+
+    public void editAbsence(Absence absence) {
+        absenceRepository.save(absence);
+    }
+
+    public void deleteAbsence(Long id) {
+        absenceRepository.deleteById(id);
+    }
 }
